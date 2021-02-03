a28ab489c2c54eb5a58a65dd608099ab2f90f319
Emma <57804718+EmmaDupuy@users.noreply.github.com>
2021-02-03 09:32:20 +0100

    Add files via upload

---
+++ CapitalistProject/src/main/java/isis/chanaldupuy/CapitalistProject/CapitalistProjectApplication.java
@@ -0,0 +1,13 @@
+package isis.chanaldupuy.CapitalistProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalistProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalistProjectApplication.class, args);
+	}
+
+}
---
+++ CapitalistProject/src/test/java/isis/chanaldupuy/CapitalistProject/CapitalistProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package isis.chanaldupuy.CapitalistProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalistProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
